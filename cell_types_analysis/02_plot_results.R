a5c9812076caeedfc50dacaf6e3b65378f60df52
Giovanni Santacatterina <65920542+jovoni@users.noreply.github.com>
2024-05-13 16:07:10 +0200

    Added cell type analysis

---
+++ cell_types_analysis/02_plot_results.R
@@ -0,0 +1,160 @@
+setwd("~/Desktop/cell_types_analysis")
+rm(list=ls())
+pkgs <- c("ggplot2", "dplyr","tidyr","tibble","reshape2")
+sapply(pkgs, require, character.only = TRUE)
+library(scMayoMap)
+library(Seurat)
+source("utils.R")
+
+set.seed(12345)
+
+dataset_name <- 'BaronPancreasData'
+
+seurat_obj <- readRDS(paste0('results/', dataset_name, '_seurat.RDS'))
+
+umap_plot_seurat <- Seurat::DimPlot(
+  seurat_obj,
+  reduction = "umap",
+  group.by = "seurat_clusters",
+  label = T,
+  repel = T) +
+  theme_minimal() +
+  theme(legend.position = 'none')
+
+umap_plot_labels <- Seurat::DimPlot(
+  seurat_obj,
+  reduction = "umap",
+  group.by = "cell_type",
+  label = T,
+  repel = T) +
+  theme_minimal() +
+  theme(legend.position = 'none')
+
+# Plot one example ####
+# m <- 'devil'
+#
+# de_res_devil <- readRDS(paste0('results/', dataset_name, '_', 'devil', '.RDS'))
+# de_res_nebula <- readRDS(paste0('results/', dataset_name, '_', 'nebula', '.RDS'))
+#
+# c <- 5
+# de_res_joint <- de_res_devil %>% dplyr::filter(cluster == c) %>%
+#   dplyr::left_join(de_res_nebula %>% dplyr::filter(cluster == c), by="name")
+#
+# plot(de_res_joint$lfc.x, de_res_joint$lfc.y)
+# plot(de_res_joint$adj_pval.x, de_res_joint$adj_pval.y)
+#
+# de_res_joint %>%
+#   dplyr::filter(lfc.x > 1, adj_pval.x <= .05, adj_pval.x > 0) %>%
+#   dplyr::arrange(-lfc.x) %>%
+#   dplyr::slice(1:50) %>%
+#   ggplot(mapping = aes(x = -log10(adj_pval.x), y = -log10(adj_pval.y))) +
+#   geom_point() +
+#   geom_abline(slope = 1, intercept = 0)
+
+m <- 'devil'
+for (m in c("devil", "nebula", "glmGamPoi")) {
+  de_res_total <- readRDS(paste0('results/', dataset_name, '_', m, '.RDS'))
+
+  input_scMayo <- prepScMayoInput(
+    de_res_total,
+    as.matrix(seurat_obj@assays$RNA$counts),
+    seurat_obj$seurat_clusters,
+    n_markers = 50,
+    lfc_cut = 1,
+    pval_cut = .05,
+    distinct_marker = FALSE) %>%
+    dplyr::mutate(gene_id = paste(gene, cluster, sep = ":"))
+
+  volc <- de_res_total %>%
+    dplyr::mutate(name_id = paste(name, cluster, sep = ":")) %>%
+    dplyr::mutate(is_marker = name_id %in% input_scMayo$gene_id) %>%
+    dplyr::mutate(adj_pval = ifelse(adj_pval <= 6.181186e-16, 6.181186e-16, adj_pval)) %>%
+    ggplot(mapping = aes(x=lfc, y=-log10(adj_pval), col=is_marker)) +
+    geom_point() +
+    scale_color_manual(values = c(alpha("black", .1), alpha("indianred", 1))) +
+    xlim(c(-5, NA)) +
+    facet_wrap(~paste0("C ",cluster), scales = "free_x", ncol = length(unique(seurat_obj$seurat_clusters))) +
+    #geom_hline(yintercept = -log10(.05), linetype = "dashed") +
+    theme_minimal() +
+    labs(x = "Fold Change (log2)", y = '-Log10 P', col="Marker") #+
+    #theme(legend.position = 'bottom')
+
+  obj <- scMayoMap(data = input_scMayo, tissue = "pancreas", pct.cutoff = 0)
+  mayoMatrix <- plot_scMayoOutput(obj) +
+    theme_minimal()
+
+  upper_row <- (umap_plot_seurat | volc) + patchwork::plot_layout(widths = c(1, 6))
+  lower_row <- (umap_plot_labels | mayoMatrix)
+
+  final_plt <- upper_row / lower_row + patchwork::plot_layout(heights = c(1, 2))
+  ggsave(paste0("plot/", dataset_name, "_", m, ".pdf"), plot = final_plt, dpi = 400, width = 12, height = 8)
+}
+
+# Extract average results ####
+whole_results <- dplyr::tibble()
+for (pval_cut in c(.05, 1e-10, 1e-50)) {
+  print(pval_cut)
+  for (n_markers in c(5, 10, 50, 100)) {
+    print(n_markers)
+    for (m in c("devil", "nebula", "glmGamPoi")) {
+      de_res_total <- readRDS(paste0('results/', dataset_name, '_', m, '.RDS'))
+
+      input_scMayo <- prepScMayoInput(de_res_total, as.matrix(seurat_obj@assays$RNA$counts), seurat_obj$seurat_clusters,
+                                      n_markers = n_markers, lfc_cut = 1, pval_cut = pval_cut, distinct_marker = FALSE)
+
+      scMayoObj <- scMayoMap(data = input_scMayo, tissue = "pancreas", pct.cutoff = 0)
+
+      ground_truth <- computeGroundTruth(seurat_obj)
+
+      pred_res <- lapply(1:nrow(scMayoObj$res), function(i) {
+        r <- scMayoObj$res[i,]
+        s <- strsplit(r$ES.norm, split = ";") %>% unlist() %>% as.numeric()
+        c <- strsplit(r$celltype, split = ";") %>% unlist()
+        c <- lapply(c, function(x) {stringr::str_replace_all(x, "cell", "")}) %>% unlist()
+        c <- lapply(c, function(x) {stringr::str_replace_all(tolower(x), " ", "")}) %>% unlist()
+        s
+        dplyr::tibble(cell_type_pred = c, score = s, cluster = r$cluster)
+      }) %>% do.call('bind_rows', .)
+
+      pred_res <- pred_res %>%
+        dplyr::group_by(cluster) %>%
+        dplyr::filter(score == max(score)) %>%
+        dplyr::mutate(n = n()) %>%
+        dplyr::mutate(cell_type_pred = ifelse(n == 1, cell_type_pred, "None")) %>%
+        dplyr::mutate(score = ifelse(n == 1, score, 0)) %>%
+        dplyr::distinct(cell_type_pred, score, cluster) %>%
+        dplyr::left_join(ground_truth, by='cluster')
+
+      pred_res$final_score <- lapply(1:nrow(pred_res), function(i) {
+        r <- pred_res[i,]
+        if (grepl(r$cell_type_pred, r$true_cell_type)) {
+          return(r$score)
+        } else {
+          return(0)
+        }
+      }) %>% unlist()
+
+      pred_res <- pred_res %>%
+        dplyr::select(cell_type_pred, cluster, true_cell_type, final_score) %>%
+        dplyr::mutate(model = m, pval_cut=pval_cut, n_markers=n_markers)
+
+      whole_results <- dplyr::bind_rows(whole_results, pred_res)
+    }
+  }
+}
+
+n_clusters <- length(unique(seurat_obj$seurat_clusters))
+
+whole_results %>%
+  group_by(model, pval_cut, n_markers) %>%
+  summarise(ACC = sum(final_score) / n_clusters) %>%
+  ggplot(mapping = aes(x=as.factor(n_markers), y=ACC, fill=model)) +
+  geom_boxplot() +
+  ggtitle("la bellezza è orribile") +
+  theme_bw()
+
+whole_results %>%
+  group_by(model, pval_cut, n_markers) %>%
+  summarise(IR = sum(final_score == 0) / n_clusters) %>%
+  ggplot(mapping = aes(x=as.factor(n_markers), y=IR, fill=model)) +
+  geom_boxplot()
---
+++ cell_types_analysis/utils.R
@@ -0,0 +1,236 @@
+
+DATASET_NAMES <- c("BaronPancreasData", 'ZhaoImmuneLiverDataBlood', 'DarmanisBrainData', 'ZhaoImmuneLiverDataLiver')
+
+read_data <- function(dataset_name) {
+  if (dataset_name == "BaronPancreasData") {
+    data <- scRNAseq::BaronPancreasData(which = "human")
+    metadata <- data@colData
+    metadata$cell_type <- metadata$label
+    tissue <- "pancreas"
+    counts <- data@assays@data[[1]]
+  } else if (dataset_name == "DarmanisBrainData") {
+    data <- scRNAseq::DarmanisBrainData()
+    metadata <- data@colData
+    metadata$cell_type <- metadata$cell.type
+    metadata$donor <- metadata$experiment_sample_name
+    tissue <- "brain"
+    counts <- data@assays@data[[1]]
+  } else if (dataset_name == 'ZhaoImmuneLiverDataBlood') {
+    data <- scRNAseq::ZhaoImmuneLiverData()
+
+    filter_cell <- grepl('blood', data$sample)
+    metadata <- dplyr::tibble(
+      cell_type = data$fine[filter_cell],
+      donor = data$sample[filter_cell]
+    )
+    counts <- data@assays@data[[1]]
+    counts <- counts[,filter_cell]
+    tissue <- "blood"
+  } else if (dataset_name == 'ZhaoImmuneLiverDataLiver') {
+    data <- scRNAseq::ZhaoImmuneLiverData()
+
+    filter_cell <- grepl('liver', data$sample)
+    metadata <- dplyr::tibble(
+      cell_type = data$fine[filter_cell],
+      donor = data$sample[filter_cell]
+    )
+    counts <- data@assays@data[[1]]
+    counts <- counts[,filter_cell]
+    tissue <- "liver"
+  } else {
+    stop("Dataset name not recognized")
+  }
+
+  return(list(counts=counts, metadata=metadata, tissue=tissue))
+}
+
+prep_seurat_object <- function(input_data, NPC=50, cluster_res=1) {
+  counts <- input_data$counts
+  metadata <- input_data$metadata
+  metadata$cell_type %>% unique()
+  metadata$donor %>% unique()
+
+  # Filter cells
+  total_counts <- colSums(counts)
+  total_features <- colSums(counts > 0)
+
+  mad5_filter <- total_counts > median(total_counts) + 5 * mad(total_counts)
+  feat100_filter <- total_features < 100
+  feat_mad_filter <- total_features > 5 * mad(total_features)
+
+  mitocondrial_genes <- grepl("^MT-", rownames(counts))
+  mitocondiral_prop <- colSums(counts[mitocondrial_genes, ]) / colSums(counts)
+  mit_prop_filter <- mitocondiral_prop > .1
+
+  # ribosomal_genes <- grepl("^RPS", rownames(counts)) | grepl("^RPL", rownames(counts))
+  # ribosomal_prop <- colSums(counts[ribosomal_genes, ]) / colSums(counts)
+  # rib_prop_filter <- ribosomal_prop < .1
+
+  #cell_outliers_filter <- mad5_filter | feat100_filter | feat_mad_filter | rib_prop_filter | mit_prop_filter
+  cell_outliers_filter <- mad5_filter | feat100_filter | feat_mad_filter |  mit_prop_filter
+
+  counts <- counts[, !cell_outliers_filter]
+  metadata <- metadata[!cell_outliers_filter, ]
+  rownames(metadata) <- colnames(counts)
+
+  # Filter genes
+  non_expressed_genes <- rowMeans(counts) <= 0.01
+  counts <- counts[!non_expressed_genes, ]
+
+  seurat_obj = SeuratObject::CreateSeuratObject(counts = counts, meta.data = as.tibble(metadata))
+  seurat_obj <- NormalizeData(seurat_obj) %>%
+    FindVariableFeatures() %>%
+    ScaleData() %>%
+    RunPCA(npcs = NPC) %>%
+    FindNeighbors(dims = 1:NPC) %>%
+    FindClusters(resolution = cluster_res) %>%
+    RunUMAP(dims=1:NPC)
+
+  new_clusters <- as.numeric(seurat_obj$seurat_clusters)
+  new_clusters <- factor(new_clusters, levels = sort(unique(new_clusters)))
+  seurat_obj$seurat_clusters <- new_clusters
+
+  seurat_obj
+}
+
+perform_analysis <- function(seurat_obj, method = "devil") {
+  if (!(method %in% c('devil', 'nebula', "glmGamPoi"))) {stop('method not recognized')}
+
+  if (method == 'devil') {
+    c <- 1
+    whole_res <- lapply(unique(seurat_obj$seurat_clusters), function(c) {
+      print(c)
+
+      design_matrix <- model.matrix(~group, dplyr::tibble(group = seurat_obj$seurat_clusters == c))
+      counts <- as.matrix(seurat_obj@assays$RNA$counts)
+
+      gg <- ((counts[,seurat_obj$seurat_clusters == c] %>% rowSums()) == 0)
+      bad_genes <- gg[gg == T] %>% names()
+      counts <- counts[!(rownames(counts) %in% bad_genes),]
+
+      fit <- devil::fit_devil(as.matrix(counts), design_matrix, verbose = T, size_factors = T)
+
+      clusters <- as.numeric(as.factor(seurat_obj$donor))
+      res <- devil::test_de(fit, contrast = c(0,1), clusters = clusters, max_lfc = Inf)
+      res %>% dplyr::mutate(cluster = c)
+
+    }) %>% do.call("bind_rows", .)
+  } else if (method == 'nebula') {
+    c <- 1
+    whole_res <- lapply(unique(seurat_obj$seurat_clusters), function(c) {
+      print(c)
+
+      design_matrix <- model.matrix(~group, dplyr::tibble(group = seurat_obj$seurat_clusters == c))
+      counts <- as.matrix(seurat_obj@assays$RNA$counts)
+
+      gg <- ((counts[,seurat_obj$seurat_clusters == c] %>% rowSums()) == 0)
+      bad_genes <- gg[gg == T] %>% names()
+      counts <- counts[!(rownames(counts) %in% bad_genes),]
+
+      clusters <- as.numeric(as.factor(seurat_obj$donor))
+      fit <- nebula::nebula(as.matrix(counts), id = clusters, pred = design_matrix)
+
+      res <- dplyr::tibble(
+        name = fit$summary$gene,
+        pval = fit$summary$p_groupTRUE,
+        adj_pval = p.adjust(fit$summary$p_groupTRUE, "BH"),
+        lfc=fit$summary$logFC_groupTRUE
+      )
+
+      res %>% dplyr::mutate(cluster = c)
+    }) %>% do.call("bind_rows", .)
+  } else if (method == "glmGamPoi") {
+    whole_res <- lapply(unique(seurat_obj$seurat_clusters), function(c) {
+      print(c)
+
+      design_matrix <- model.matrix(~group, dplyr::tibble(group = seurat_obj$seurat_clusters == c))
+      counts <- as.matrix(seurat_obj@assays$RNA$counts)
+
+      gg <- ((counts[,seurat_obj$seurat_clusters == c] %>% rowSums()) == 0)
+      bad_genes <- gg[gg == T] %>% names()
+      counts <- counts[!(rownames(counts) %in% bad_genes),]
+
+      fit <- glmGamPoi::glm_gp(as.matrix(counts), design_matrix, size_factors = T, verbose = T)
+      res <- glmGamPoi::test_de(fit, contrast = c(0,1), max_lfc = Inf)
+      res <- res %>% select(name, pval, adj_pval, lfc)
+
+      res %>% dplyr::mutate(cluster = c)
+
+    }) %>% do.call("bind_rows", .)
+  }
+  whole_res
+}
+
+prepScMayoInput <- function(de_res, count_matrix, seurat_clusters, n_markers=50, lfc_cut=1, pval_cut=1e-10, distinct_marker=TRUE) {
+  if (!(all(colnames(de_res) == c("name", "pval", "adj_pval", "lfc", "cluster")))) {stop('input de_res have wrong column names')}
+
+  cluster_values <- de_res$cluster %>% unique()
+
+  scMayoInput <- dplyr::tibble()
+  for (c in cluster_values) {
+    top_res <- de_res %>%
+      dplyr::filter(cluster == c, lfc > lfc_cut, adj_pval <= pval_cut) %>%
+      dplyr::arrange(-lfc) %>%
+      dplyr::slice(1:n_markers)
+
+    gene_percentage <- dplyr::tibble()
+    for (gene_name in top_res$name) {
+      g_counts <- count_matrix[gene_name, seurat_clusters == c]
+      non_g_counts <- count_matrix[gene_name, !(seurat_clusters == c)]
+
+      pct.1 <- sum(g_counts > 0) / length(g_counts)
+      pct.2 <- sum(non_g_counts > 0) / length(non_g_counts)
+
+      gene_percentage <- dplyr::bind_rows(gene_percentage, dplyr::tibble(name = gene_name, pct.1=pct.1, pct.2=pct.2))
+    }
+
+    top_res <- top_res %>% dplyr::left_join(gene_percentage, by="name")
+    scMayoInput <- dplyr::bind_rows(scMayoInput, top_res)
+  }
+
+  scMayoInput <- scMayoInput %>%
+    dplyr::rename(gene=name, p_val_adj=adj_pval, p_val=pval, pct.1=pct.1, pct.2=pct.2, cluster=cluster, avg_log2FC=lfc)
+
+  if (distinct_marker) {
+    input_scMayo$n <- lapply(input_scMayo$gene, function(x) { sum(input_scMayo$gene == x) }) %>% unlist()
+    input_scMayo <- input_scMayo %>% dplyr::filter(n == 1) %>% dplyr::select(!n)
+  }
+  #rownames(scMayoInput) <- scMayoInput$gene
+  scMayoInput
+}
+
+plot_scMayoOutput <- function(scMayoObj) {
+  pred_res <- lapply(1:nrow(scMayoObj$res), function(i) {
+    r <- scMayoObj$res[i,]
+    s <- strsplit(r$ES.norm, split = ";") %>% unlist() %>% as.numeric()
+    c <- strsplit(r$celltype, split = ";") %>% unlist()
+    c <- lapply(c, function(x) {stringr::str_replace_all(x, "cell", "")}) %>% unlist()
+    c <- lapply(c, function(x) {stringr::str_replace_all(tolower(x), " ", "")}) %>% unlist()
+    s
+    dplyr::tibble(cell_type_pred = c, score = s, cluster = r$cluster)
+  }) %>% do.call('bind_rows', .)
+
+  ground_truth <- computeGroundTruth(seurat_obj)
+
+  ground_truth %>%
+    dplyr::left_join(pred_res, by="cluster") %>%
+    dplyr::mutate(ground_truth = as.factor(paste0(true_cell_type, ' (', cluster, ')'))) %>%
+    ggplot(mapping = aes(x=cell_type_pred, y=ground_truth, fill=score)) +
+    geom_tile() +
+    scale_fill_continuous(type = "viridis") +
+    theme_bw() +
+    labs(y = "Annotated cell type", x = "Assigned cell type", fill="Score")
+}
+
+computeGroundTruth <- function(seurat_obj) {
+  lapply(unique(seurat_obj$seurat_clusters), function(c) {
+    table_c <- seurat_obj@meta.data %>%
+      dplyr::filter(seurat_clusters == c) %>%
+      dplyr::pull(cell_type) %>%
+      table()
+    table_c <- table_c / sum(table_c)
+    max_idx <- which.max(table_c)
+    dplyr::tibble(cluster = c, true_cell_type = names(table_c)[max_idx], pct = table_c[max_idx])
+  }) %>% do.call('bind_rows', .)
+}
+
