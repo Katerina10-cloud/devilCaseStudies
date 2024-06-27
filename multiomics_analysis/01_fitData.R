680ea6a7bea49c97bc2f7fa450d9a6a67edddc5a
Katsiaryna Davydzenka <katsiarynadavydzenka@cli-10-109-0-174.WIFIeduroamEXT.units.it>
2024-06-27 11:59:38 +0200

    mc

--- multiomics_analysis/01_fitData.R
+++ multiomics_analysis/01_fitData.R
@@ -8,7 +8,7 @@ source("utils_multiomics.R")
 set.seed(12345)
 
 ## Input data
-data_path <- "/data/seurat_rna_muscl.RDS"
+data_path <- "data/multiomics/seurat_muscl_rna.RDS"
 dataset_name <- "MuscleRNA"
 
 #data_path <- "data/atac_age.RDS"
@@ -23,26 +23,26 @@ input_data <- prepare_rna_input(input_data)
 #input_data <- prepare_atac_input(input_data)
 
 # RNA analysis #
-time <- dplyr::tibble()
-m <- 'devil'
-for (m in c("devil", "nebula", "glmGamPoi")) {
+#time <- dplyr::tibble()
+m <- 'nebula'
+for (m in c("nebula")) {
   s <- Sys.time()
   de_res <- perform_analysis_rna(input_data, method = m)
-  e <- Sys.time()
+  #e <- Sys.time()
   saveRDS(de_res, paste0('results/', dataset_name, '/', m, '_rna', '.RDS'))
-  time <- dplyr::bind_rows(time, dplyr::tibble(method = m, delta_time = e - s))
-  print(time)
+  #time <- dplyr::bind_rows(time, dplyr::tibble(method = m, delta_time = e - s))
+  #print(time)
 }
 
 # ATAC analysis #
-time <- dplyr::tibble()
-m <- 'devil'
-for (m in c("devil", "nebula", "glmGamPoi")) {
-  s <- Sys.time()
-  de_res <- perform_analysis_atac(input_data, method = m)
-  e <- Sys.time()
-  saveRDS(de_res, paste0('results/', dataset_name, '/', m, '_atac', '.RDS'))
-  time <- dplyr::bind_rows(time, dplyr::tibble(method = m, delta_time = e - s))
-  print(time)
-}
+#time <- dplyr::tibble()
+#m <- 'devil'
+#for (m in c("devil", "nebula", "glmGamPoi")) {
+  #s <- Sys.time()
+  #de_res <- perform_analysis_atac(input_data, method = m)
+  #e <- Sys.time()
+  #saveRDS(de_res, paste0('results/', dataset_name, '/', m, '_atac', '.RDS'))
+  #time <- dplyr::bind_rows(time, dplyr::tibble(method = m, delta_time = e - s))
+  #print(time)
+#}
 
