e98def187fbc1b5f6bf9a3ef83efb7af6ae4c2c8
Giovanni Santacatterina <gsantacatterina@login02.hpc.rd.areasciencepark.it>
2024-11-18 19:03:09 +0100

    mc

--- timing_scaling/run_cpu_devil.sh
+++ timing_scaling/run_cpu_devil.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-#SBATCH --mem=1000GB
+#SBATCH --mem=400GB
 #SBATCH --time=18:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=24
-#SBATCH --partition=THIN
-#SBATCH --output=out/de_cpu.log
-#SBATCH --job-name=cpu_all
+#SBATCH --partition=EPYC
+#SBATCH --output=out/de_cpu_devil.log
+#SBATCH --job-name=devil_cpu
 
 module load R
 
 # export R_LIBS_USER=~/scratch/r_packages_gpu
 
 Rscript scripts/cpu_scaling_test_devil.R
-Rscript scripts/cpu_scaling_test_glm.R
